fbbde7699613b2f898824eac77664b6f0e78d0a0
nikhileshprovenir <nghantasala@gmail.com>
2015-08-10 00:55:16 -0400

    Learning 1

---
+++ ember-project-Learning-from-Joshua/Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+ruby "2.1.5"
+
+gem 'rails', '4.2.1'
+gem 'pg'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.1.0'
+gem 'jquery-rails'
+gem 'jbuilder', '~> 2.0'
+gem 'puma'
+gem 'foreman'
+gem 'ember-rails'
+gem 'ember-source'
+gem 'emblem-rails'
+gem 'rails_12factor'
+gem 'pry-rails'
---
+++ ember-project-Learning-from-Joshua/app/assets/javascripts/application.js
@@ -0,0 +1,18 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+<script src="app/assets/javascripts/jquery.maskedinput-1.3.js"></script>
+ <script src="app/assets/javascripts/jquery-ui-1.8.16.custom.min.js"></script>
